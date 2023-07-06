c40d95a09c61a3bfd07e21cf35325e616b9bf0c8
weavercoder <137697089+weavercoder@users.noreply.github.com>
2023-07-06 15:01:58 -0400

    first commit

---
+++ code_one.py
@@ -0,0 +1,10 @@
+name = input("enter your name: ")
+print(f("Hello! , {name}"))
+marital_status = input("are you married, (yes or no)")
+    if marital_status.lower() == "yes"
+    spouse_no = input("enter your spouse number: ")
+    print(f("spouse number: ,{spouse_no}"))
+    else 
+    own_no = input("enter your own number: ")
+    print(f("own number: ,{own_no}: "))
+    
